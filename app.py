2140ac1dda32295226d0a4396b0ce41da26071da
cinmoy98 <cinmoy98@gmail.com>
2020-06-09 21:24:39 +0600

    fixing 3

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 import matplotlib.pyplot as plt
 import numpy as np
 
-URI = 'https://neural-net-visualizer.herokuapp.com/'
+URI = 'http://127.0.0.1:5000'
 
 st.title('Nural Network Visualizer')
 st.sidebar.markdown('## Input Image')
