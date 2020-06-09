ccb84abc62797270768e2f454b9b63b2a1b7d47e
cinmoy98 <cinmoy98@gmail.com>
2020-06-09 23:59:36 +0600

    fixing 7

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn -w 2 -b 0.0.0.0:8000 ml_server:app
+web: gunicorn -b 0.0.0.0:8000 ml_server:app
 web: sh setup.sh && streamlit run app.py
--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 import matplotlib.pyplot as plt
 import numpy as np
 
-URI = 'http://127.0.0.1:5000'
+URI = 'http://neural-net-visualizer.herokuapp.com:8000/'
 
 st.title('Nural Network Visualizer')
 st.sidebar.markdown('## Input Image')
